eae5bb951a5f2b95c722fab817a660cbdc808f20
Misha <zigelboim.misha@gmail.com>
2023-06-14 16:01:18 +0300

    Receiver µservice added to the monorepo

--- README.md
+++ README.md
@@ -9,15 +9,39 @@ One way this can be achieved is by exporting Prometheus metrics and create a KPI
 Registering a new CI job will result in the following metrics:
 
 ```prometheus
+# HELP ci_dotnet_build_total The number of dotnet build used
+# TYPE ci_dotnet_build_total counter
+ci_dotnet_build_total{project="dummy-project",status="success"} 3
+ci_dotnet_build_total{project="dummy-project2",status="success"} 5
+ci_dotnet_build_total{project="dummy-project2",status="failed"} 12
+ci_dotnet_build_total{project="dummy-project3",status="success"} 8
 
+# HELP ci_helm_restore_duration The duration of the helm restore usage
+# TYPE ci_helm_restore_duration histogram
+ci_helm_restore_duration_bucket{status="success",le="60"} 60
+ci_helm_restore_duration_bucket{status="success",le="120"} 67
+ci_helm_restore_duration_bucket{status="success",le="300"} 68
+ci_helm_restore_duration_bucket{status="success",le="600"} 72
+ci_helm_restore_duration_bucket{status="success",le="900"} 74
+ci_helm_restore_duration_bucket{status="success",le="1200"} 75
+ci_helm_restore_duration_bucket{status="success",le="+Inf"} 76
+ci_helm_restore_duration_sum_{status="success"} 7005.646230537002
 ```
 
-## Sending Metrics
+## How Does It Work
 
-This µservice waits for `API` requests to register metrics:
+1. User runs a CI job
 
-```cmd
-curl -G -d started=2023-05023T13:01:00Z -d status=failed -d project=dummy-project -d name=dotnet_build http://localhost:80/steps
-```
+1. Jobs scripts are completed
 
-This will register the new CI job (if it's the first time its used) and increase it accordingly.
+1. `after_scripts` saves the metrics into a `.json` file in the mounted volume
+
+1. Sidecar reads the `.json` file and sends it to the `Receiver µservice`
+
+1. The `Receiver µservice` increases the correct `metrics` of the complete job
+
+## µServices
+
+- [Receiver](/receiver/README.md)
+
+- [Exporter](/exporter/README.md)
---
+++ receiver/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	allStepsCounter           = map[string]*prometheus.CounterVec{}
+	allStepsDurationHistogram = map[string]*prometheus.HistogramVec{}
+)
+
+// handleRequest registers and increases metrics for each CI job request that arrives
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	project, status, name, duration := getParameters(r)
+
+	splitJobName := strings.Split(name, "_")
+
+	_, ok := allStepsCounter[fmt.Sprintf("ci_%s_%s_total", splitJobName[0], splitJobName[1])]
+	// If key already registered
+	if ok {
+		increaseMetric(splitJobName[0], splitJobName[1], status, project, duration)
+	} else {
+		registerNewJob(splitJobName[0], splitJobName[1])
+		increaseMetric(splitJobName[0], splitJobName[1], status, project, duration)
+	}
+}
+
+// increaseMetric increasing both Counter and Histogram metrics for the received CI job
+func increaseMetric(framework, step, status, project string, duration float64) {
+	allStepsCounter[fmt.Sprintf("ci_%s_%s_total", framework, step)].WithLabelValues(status, project).Add(1)
+	allStepsDurationHistogram[fmt.Sprintf("ci_%s_%s_duration", framework, step)].WithLabelValues(status).Observe(duration)
+}
+
+// getParameters fetches the parameters from the URL and returns them
+func getParameters(r *http.Request) (string, string, string, float64) {
+	status := r.URL.Query().Get("status")
+	project := r.URL.Query().Get("project")
+	name := r.URL.Query().Get("name")
+	started := r.URL.Query().Get("started")
+
+	startedParsed := strings.Replace(started, "T", " ", -1)
+	startedParsed = strings.Replace(startedParsed, "Z", "", -1)
+	startedParsed = strings.Replace(startedParsed, "-", ".", -1)
+	startedAt := time.Parse("2006.01.02 15.04.05", startedParsed)
+	duration := time.Now().Sub(startedAt).Seconds()
+
+	fmt.Println(fmt.Printf("%f %s %s %s", duration, status, project, name))
+	return project, status, name, duration
+}
+
+// registerNewJob registers 2 metrics for each new CI job arrives to this µservice
+func registerNewJob(framework, step string) {
+	fmt.Println(fmt.Sprintf("Registering metrics for: %s-%s", framework, step))
+	allStepsCounter[fmt.Sprintf("ci_%s_%s_total", framework, step)] = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: fmt.Sprintf("ci_%s_%s_total", framework, step),
+		Help: fmt.Sprintf("The number of %s %s used", framework, step),
+	},
+		[]string{"status", "project"},
+	)
+	allStepsDurationHistogram[fmt.Sprintf("ci_%s_%s_duration", framework, step)] = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    fmt.Sprintf("ci_%s_%s_duration", framework, step),
+		Help:    fmt.Sprintf("The duration of %s %s uses", framework, step),
+		Buckets: []float64{60, 120, 300, 600, 900, 1200},
+	},
+		[]string{"status"},
+	)
+}
+
+func whileTrue() {
+	for {
+	}
+}
+
+func main() {
+	http.HandleFunc("/jobs", handleRequest)
+
+	http.Handle("/", promhttp.Handler())
+	http.ListenAndServe(":80", nil)
+
+	go whileTrue()
+}
